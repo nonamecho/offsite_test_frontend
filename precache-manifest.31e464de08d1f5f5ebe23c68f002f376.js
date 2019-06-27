08e655aee036dd7c9ec4696c600e358c80ae80c6
nonamecho <joccw@hotmail.com>
2019-06-27 23:42:02 +0800

    Updates

---
+++ precache-manifest.31e464de08d1f5f5ebe23c68f002f376.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbb7a73518b893b7a92c3be65725506",
+    "url": "/offsite_test_frontend/index.html"
+  },
+  {
+    "revision": "c541db2c178263e333d3",
+    "url": "/offsite_test_frontend/static/js/2.c7ba4d4e.chunk.js"
+  },
+  {
+    "revision": "38f8d0975b36d9e8f950",
+    "url": "/offsite_test_frontend/static/js/main.181e573f.chunk.js"
+  },
+  {
+    "revision": "4383615ef9ccfe9d5ea3",
+    "url": "/offsite_test_frontend/static/js/runtime~main.43436cec.js"
+  }
+]);
